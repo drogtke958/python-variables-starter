018a02869bcfe9c91ce161a344c07f20c613ab30
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-27 08:18:20 -0400

    Update python_f_strings.py

--- python_f_strings.py
+++ python_f_strings.py
@@ -1,6 +1,6 @@
 # Programmer: Bruce Provencher
-# Date: 13 AUG 2018
-# Project: Python F-Strings Example
+# Date: 13 AUG 20XX
+# Project: Python F-Strings 
 
 # A Python-style comment
 
@@ -9,5 +9,5 @@ car1 = "Chevrolet"
 car2 = "Honda"
 down_payment = 2500.0000000
 
-print(f'Yesterday, I visited the car dealer and test drove a {car1} and a {car2}.')
+print(f'Last Saturday, I went to the car dealer and test drove a {car1} and a {car2}.')
 print(f"I needed ${down_payment:,.2f} for a down payment, but unfortunately, I didn't have that much on me.")
