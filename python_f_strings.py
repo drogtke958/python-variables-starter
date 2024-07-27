d501f90564fa577577994fadb616c9880f6c315e
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-27 08:29:18 -0400

    Update python_f_strings.py

--- python_f_strings.py
+++ python_f_strings.py
@@ -2,9 +2,17 @@
 # 13 AUG 20XX
 # Python F-Strings 
 
+# How do you create a string in Python?
 car1 = "Chevrolet Malibu"
 car2 = "Honda Accord"
+
+# Is the value on the next line of code an integer or a floating-point number (float)?
 down_payment = 2500.0000000
 
+# Predict the output
+print(car2)
+print(down_payment)
+
+# Predict the output
 print(f'Last Saturday, I went to two car dealers and test drove a {car1} and a {car2}.')
 print(f"I needed ${down_payment:,.2f} for a down payment on the Honda.")
