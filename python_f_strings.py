dec217f0ab728165bb3e83e5a7281f48d7311782
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-27 08:33:24 -0400

    Update python_f_strings.py

--- python_f_strings.py
+++ python_f_strings.py
@@ -15,5 +15,6 @@ print(car2)
 print(down_payment)
 
 # Predict the output
+# How is the f-string set up differently in the examples below?
 print(f'Last Saturday, I went to two car dealers and test drove a {car1} and a {car2}.')
 print(f"I needed ${down_payment:,.2f} for a down payment on the {car2}, which had only {mileage} miles on it.")
