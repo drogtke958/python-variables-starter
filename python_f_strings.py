7722ce1c213ea8222a77d48d1c57ac8f08692dc0
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-27 08:25:49 -0400

    Update python_f_strings.py

--- python_f_strings.py
+++ python_f_strings.py
@@ -6,5 +6,5 @@ car1 = "Chevrolet"
 car2 = "Honda"
 down_payment = 2500.0000000
 
-print(f'Last Saturday, I went to the car dealer and test drove a {car1} and a {car2}.')
+print(f'Last Saturday, I went to two car dealers and test drove a {car1} and a {car2}.')
 print(f"I needed ${down_payment:,.2f} for a down payment, but unfortunately, I didn't have that much money on me.")
