d4275f62f0b5b75f607056d1f8336c06d4944897
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-27 08:55:35 -0400

    Create data-types-practice.py

---
+++ data-types-practice.py
@@ -0,0 +1,33 @@
+# Student name
+# 04 SEP 2024
+# Variables & Data Types Practice
+
+# Demonstrate in this script that you know how to
+# define (create) Python variables and
+# assign string and numeric values to your variables
+
+# Remember:
+# A string is just a piece of text (Examples: Mikey, Traverse City, hot dog)
+# An integer is a number that doesn't have a decimal point (Example: 42)
+# A float (AKA floating-point number) is a number that does have a decimal point (Example: 3.2537)
+
+# Part 1 / Strings
+# Define a variable called school and assign it the string value Career Tech
+# Define a variable called favorite_singer and assign it the string value that is your favorite singer
+# Define a variable called greeting and assign it a string value you'd use to greet someone
+
+
+
+
+# Part 2 / Numeric Data (Numbers)
+# Define a variable called my_age and assign it your age as an integer
+# Define a variable called pi and assign it the value of 3.1415926 (a float/floating-point number)
+# Define a variable called room_length and assign it a value of 20.75 (a float/floating-point number)
+
+
+
+
+# Part 3 / F-Strings
+# Build an f-string that displays a custom greeting to your best friend
+# Build an f-string that tells us your first name and the name of the school you attend
+# Build an f-string that tells us in a sentence what the value of pi is.
