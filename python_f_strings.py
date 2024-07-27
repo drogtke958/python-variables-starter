791e593acb9aa7ed7dd18b2276f2402b9ba1d4a9
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-27 08:26:10 -0400

    Update python_f_strings.py

--- python_f_strings.py
+++ python_f_strings.py
@@ -2,8 +2,8 @@
 # 13 AUG 20XX
 # Python F-Strings 
 
-car1 = "Chevrolet"
-car2 = "Honda"
+car1 = "Chevrolet Malibu"
+car2 = "Honda Accord"
 down_payment = 2500.0000000
 
 print(f'Last Saturday, I went to two car dealers and test drove a {car1} and a {car2}.')
