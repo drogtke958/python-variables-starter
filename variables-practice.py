58059275cfdd8ace67c6ef58ec02590d225e2d0b
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-09-06 07:29:23 -0400

    Update and rename data-types-practice.py to variables-practice.py

--- variables-practice.py
+++ variables-practice.py
@@ -2,15 +2,15 @@
 # 04 SEP 2024
 # Variables & Data Types Practice
 
-# Demonstrate in this script that you know how to
+# Demonstrate in this script that you know how to:
 # define (create) Python variables and
 # assign string and numeric values to your variables
-# build an f-string
+# use the Python print function to print the value stored in a variable on your computer
 
 # Remember:
 # A string is just a piece of text (Examples: Mikey, Traverse City, hot dog)
 # An integer is a number that doesn't have a decimal point (Example: 42)
-# A float (AKA floating-point number) is a number that does have a decimal point (Example: 3.2537)
+# A float (AKA floating-point number) is a number that DOES have a decimal point (Example: 3.2537)
 
 # Part 1 / Strings
 # Define a variable called school and assign it the string value Career Tech (using a pair of single quotes)
@@ -22,13 +22,13 @@
 
 # Part 2 / Numeric Data (Numbers)
 # Define a variable called my_age and assign it your age as an integer
-# Define a variable called pi and assign it the value of 3.1415926 (a float/floating-point number)
+# Define a variable called conversion_factor and assign it the value of 2.54 (a float/floating-point number)
 # Define a variable called room_length and assign it a value of 20.75 (a float/floating-point number)
 
 
 
 
-# Part 3 / Working with f-strings
-# Build an f-string that displays a custom greeting to your best friend
-# Build an f-string that tells us your first name and the name of the school you attend
-# Build an f-string that tells us in a sentence what the value of pi is.
+# Part 3 / Using the Python print Function
+# Use the print function to print your first name on your screen
+# Use the print function to print the name of the city you live in on your screen
+# Use the print function to print your age on your computer screen
